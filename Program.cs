7c4554df62565c8806dab9d5e73928c50b01e909
12 <12@DESKTOP-K6U2P3E>
2023-12-22 10:00:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите время в секундах");
+int t = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ускорение");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость в начальный момент времени");
+int v = Convert.ToInt32(Console.ReadLine());
+double s = v * t + (a * Math.Pow(t, 2)) / 2;
+Console.WriteLine($"Путь будет равен {s}");
